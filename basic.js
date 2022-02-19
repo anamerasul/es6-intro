83e99cc7a38d5110d9edbc861a5dd459c2e680c5
anamerasul <anamerasul@gmail.com>
2022-02-19 11:52:57 +0600

    es6 intro basic js

---
+++ basic.js
@@ -0,0 +1,65 @@
+// variable
+var deposit= 400;
+// var let const
+
+if(deposit<500){
+
+}
+
+else if(deposit>200){
+
+}
+
+else if(deposit==500){
+
+}
+
+else if(deposit !=500){
+
+}
+
+else if(deposit<=500){
+
+}
+
+else if(deposit>=200){
+
+}
+
+// array
+const number =[45,6587,124,45,1];
+
+const numberCount=number.length
+
+number.push(111);
+
+number.pop();
+number[2]=555;
+
+// check whether 222 included in the array
+
+if(number.indexOf(222)!=-1){
+
+}
+
+else if(number.includes(222)==true){
+
+}
+
+// loop
+
+// while loop for loop
+
+for(const num of number){
+
+}
+// function
+function fullName(first, second){
+        const name =first+' '+second
+        return name
+}
+
+const person =fullName('rahim','ali');
+
+// object
+const bottle={color:'yellow',contain:'water',price:340}
