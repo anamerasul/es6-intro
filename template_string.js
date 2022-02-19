3f74f70c23f6b5304e6d8ca3fd8bccf527bf69a4
anamerasul <anamerasul@gmail.com>
2022-02-19 14:43:47 +0600

    Template string, multiple line string, dynamic string

---
+++ template_string.js
@@ -0,0 +1,30 @@
+const priya='asif akbar';
+const meye ="meye tumi ki dukhho";
+
+const kobita=` kobita tumi sopno`
+
+const multiLine ='this is my first line \
+this is my second line\n\
+ this is '
+
+const multiLineNew=`this is multi line
+tyryeegy`
+
+// console.log(multiLine)
+console.log(multiLineNew)
+
+const friends=['abul','babul','ramim','tamim']
+
+const friendCount=5;
+
+const old=` <h3 class="friend-name">friends-new</h3>`
+const old2='<h3 class="friend-name">friends'+friendCount+ '</h3>'
+
+const new1=` <h3 class="friend-name">friends-${friendCount}+${friends.length}</h3>`
+console.log(new1);
+
+const first='tonmoy';
+const last ='anam';
+
+const fullNew=`this person name: ${first} ${last} has money ${friends.length*400}`
+console.log(fullNew)
